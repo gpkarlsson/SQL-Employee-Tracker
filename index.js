87ffd6c5c8bda05c15b4336d3095328b3934096f
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-02-17 11:04:25 -0600

    working on basic js and sql schema

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const console_table = require('console.table');
+const util = require('util');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
+
+connection.query = util.promisify(connection.query);
